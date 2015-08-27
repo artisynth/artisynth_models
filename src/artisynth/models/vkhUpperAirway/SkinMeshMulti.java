8aae127e1edfa8a00231264b1798846a975f04ed
John Lloyd <lloyd@cs.ubc.ca>
2015-08-27 00:23:38 +0000

    small changes to setFromElement and setFromFem for Point and Frame FEM attachments
    git-svn-id: https://svn.artisynth.org/svn/artisynth_models/trunk@51 069fe65c-b9ff-4292-81a4-7ac0a0cb721f

--- src/artisynth/models/vkhUpperAirway/SkinMeshMulti.java
+++ src/artisynth/models/vkhUpperAirway/SkinMeshMulti.java
@@ -349,7 +349,7 @@ public class SkinMeshMulti extends MeshComponent {
       int numWeights = 0;
       if (elem != null) {
          VectorNd coords = new VectorNd (elem.numNodes());
-         elem.getMarkerCoordinates (coords, vtx.pnt); 
+         elem.getMarkerCoordinates (coords, vtx.pnt, false); 
          for (int k=0; k<coords.size(); k++) {
             if (Math.abs(coords.get(k)) >= reduceTol) {
                nodes.add (elem.getNodes()[k]);
