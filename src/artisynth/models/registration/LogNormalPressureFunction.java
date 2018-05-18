b6ddb8006a83abdd59e72594e1d075bc5e79961a
Antonio Sanchez <antonios@ece.ubc.ca>
2018-05-18 11:07:29 -0700

    better setting of parameters with more-intuitive meaning

--- src/artisynth/models/registration/LogNormalPressureFunction.java
+++ src/artisynth/models/registration/LogNormalPressureFunction.java
@@ -1,5 +1,7 @@
 package artisynth.models.registration;
 
+import org.python.modules.math;
+
 import maspack.matrix.Point3d;
 import maspack.matrix.Vector3d;
 import maspack.properties.PropertyList;
@@ -32,6 +34,8 @@ public class LogNormalPressureFunction extends RegistrationPressureFunctionBase 
       myProps.add("maxPressure * *", "maximum registration pressure", DEFAULT_PRESSURE);
       myProps.add("sigma * *", "Gaussian sigma", DEFAULT_SIGMA);
       myProps.add("mu * *", "Gaussian mu", DEFAULT_MU);
+      myProps.add ("peak * *", "peak pressure distance", Math.exp (DEFAULT_MU - DEFAULT_SIGMA*DEFAULT_SIGMA));
+      myProps.add ("halfPeak * *", "half-peak pressure distance", Math.exp (DEFAULT_MU - DEFAULT_SIGMA*DEFAULT_SIGMA + DEFAULT_SIGMA*Math.sqrt (2*Math.log (2))));
       myProps.add("scaleByNormalDotProduct isScalingByNormalDotProduct *", "radius of target gravity sphere", DEFAULT_SCALE);
    }
    public PropertyList getAllPropertyInfo() {
@@ -58,16 +62,18 @@ public class LogNormalPressureFunction extends RegistrationPressureFunctionBase 
       
       double dist = pressure.norm();
       
-      double emu = Math.exp(mu);
+      double xpeak =Math.exp (mu - sigma*sigma);
       final double EPS = 1e-12;
       
-      if (dist <= EPS*emu) {
+      if (dist <= EPS*xpeak) {
          pressure.setZero();
          return;
       }
        
       double a = (Math.log(dist)-mu)/sigma;
-      double pnorm = (emu/dist)*Math.exp(-a*a/2); // log-normal scaled to have unit peak         
+      double b = Math.exp (mu - sigma*sigma/2);
+      
+      double pnorm = (b/dist)*Math.exp(-a*a/2); // log-normal scaled to have unit peak         
       pnorm *= maxPressure;   // scale up normalized log-normal to have specified peak pressure
       
       // scale by dot product of normals if desired
@@ -103,7 +109,87 @@ public class LogNormalPressureFunction extends RegistrationPressureFunctionBase 
    public void setMu(double m) {
       mu = m;
    }
-
+   
+   /**
+    * Determines values of mu and sigma that results in peak pressure and
+    * half of peak pressure at the provided distances
+    * 
+    * @param p peak pressure distance
+    * @param hp distance of half peak pressure
+    */
+   public void setPeakHalfPeak(double p, double hp) {
+      
+      // y1 = (mu - sigma^2) + sigma*sqrt(-2log(1)) = log(p)
+      // y2 = (mu - sigma^2) + sigma*sqrt(-2log(0.5)) = log(hp)
+      double y1 = Math.log (p);
+      double y2 = Math.log (hp);
+      
+      setLogPeakHalfPeak(y1, y2);
+      
+   }
+   
+   /**
+    * Sets the parameters mu, sigma such that the log-normal has 
+    * peak pressure at distance exp(y1) and half of peak pressure
+    * at distance exp(y2)
+    * @param y1 log of peak distance
+    * @param y2 log of half-peak distance
+    */
+   private void setLogPeakHalfPeak(double y1, double y2) {
+      double b = 2*Math.log(2);
+      double dy = Math.abs (y2 - y1);
+      double sigma = dy/Math.sqrt(b);
+      double mu = y1 + sigma*sigma;
+      setSigma (sigma);
+      setMu(mu);
+   }
+   
+   /**
+    * Determines value of mu and sigma that results in peak pressure at distance p.
+    * For this computation, the half-peak remains fixed.
+    * 
+    * @param p peak pressure distance
+    */
+   public void setPeak(double p) {
+      
+      double y1 = Math.log (p);
+      double y2 = mu - sigma*sigma + sigma*math.sqrt (2*Math.log (2));
+      
+      setLogPeakHalfPeak (y1, y2);
+   }
+   
+   /**
+    * Determines the values of mu and sigma that result in half-peak pressure at distance hp.
+    * For this computation, the peak remains fixed
+    * 
+    * @param hp distance of half of peak pressure
+    */
+   public void setHalfPeak(double hp) {
+      // y1 = (mu - sigma^2) + sigma*sqrt(-2log(1)) = log(p)
+      // y2 = (mu - sigma^2) + sigma*sqrt(-2log(0.5)) = log(hp)
+      double y1 = mu - sigma*sigma;
+      double y2 = Math.log (hp);
+      setLogPeakHalfPeak (y1, y2);
+   }
+   
+   /**
+    * Distance at which peak pressure is achieved
+    * @return peak pressure distance
+    */
+   public double getPeak() {
+      return Math.exp (mu - sigma*sigma);
+   }
+   
+   /**
+    * Distance at which half of peak pressure is achieved
+    * @return half-peak pressure distance
+    */
+   public double getHalfPeak() {
+      double y2 = mu - sigma*sigma + sigma*math.sqrt (2*Math.log (2));
+      return Math.exp (y2);
+   }
+   
+   
    public boolean isScalingByNormalDotProduct() {
       return scaleNormalDotProduct;
    }
